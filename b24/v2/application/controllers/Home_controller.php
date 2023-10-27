e74cde6dd364bb0132f29c4e0ea9b71f8c41ca65
ziso_ <zulfuisayev0@gmail.com>
2023-10-27 17:18:20 +0400

    niyebiz
    sss

--- b24/v2/application/controllers/Home_controller.php
+++ b24/v2/application/controllers/Home_controller.php
@@ -110,6 +110,33 @@ class Home_controller extends Home_Core_Controller
 		}
 	}
 
+    /**
+     * niyebiz Page
+     */
+    public function niyebiz()
+    {
+        get_method();
+        $data['page'] = $this->page_model->get_page('niyebiz');
+        //check page auth
+        $this->checkPageAuth($data['page']);
+
+        if ($data['page']->page_active == 0) {
+            $this->error_404();
+        } else {
+            $data['title'] = get_page_title($data['page']);
+            $data['description'] = get_page_description($data['page']);
+            $data['keywords'] = get_page_keywords($data['page']);
+
+
+            $this->load->view('partials/_header', $data);
+            $this->load->view('niyebiz', $data);
+            $this->load->view('partials/_footer');
+        }
+    }
+
+
+
+
 	/**
 	 * Contact Page
 	 */
